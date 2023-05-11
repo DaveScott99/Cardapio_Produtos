e38d20af47fbe028b7b7cffe298bf22cee7011e3
Davi Santos <davicalixto2077@gmail.com>
2023-05-11 20:58:08 -0300

    Component modal and Post foods

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import './App.css'
 import { Card } from './components/Card/card';
 import { useFoodData } from './hooks/useFoodData';
 import { FoodData } from './interface/FoodData';
+import { CreateModal } from './components/create-modal/create-modal';
 
 function App() {
 
   const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev)
+  }
 
   return (
     <>
@@ -21,8 +28,10 @@ function App() {
               imgUrl={foodData.imgUrl}
             />
           )}
-        </div>
 
+        </div>
+          {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+          <button onClick={handleOpenModal}>Novo</button>
       </div>
     </>
   )
