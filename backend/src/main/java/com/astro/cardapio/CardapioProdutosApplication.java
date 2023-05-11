312ee9e1b241a0f4579c3cc32e7caa4368eb904a
Davi Santos <davicalixto2077@gmail.com>
2023-05-11 17:51:46 -0300

    Project created

---
+++ backend/src/main/java/com/astro/cardapio/CardapioProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.astro.cardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioProdutosApplication.class, args);
+	}
+
+}
